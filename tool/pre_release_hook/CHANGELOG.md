da4e86fd6cf130a5a81c6cc256df4eae053e12fa
Brett Sutton <bsutton@noojee.com.au>
2022-09-03 11:42:15 +1000

    Released 1.0.4.

---
+++ tool/pre_release_hook/CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.0.4
+Added publish hooks to pin the dependency versions to ensure
+that dwhich always activates even if incompatible versions have
+been published.
+
---
+++ tool/pre_release_hook/pin.dart
@@ -0,0 +1,10 @@
+#! /usr/bin/env dcli
+
+// ignore: unused_import
+import 'package:dcli/dcli.dart';
+
+// pin the versions so dwhich is stable in in the face of changing
+// dependencies.
+void main(List<String> args) {
+  'dcli dep pin'.run;
+}
